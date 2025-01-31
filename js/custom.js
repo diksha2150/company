93f5b3de56d871c15b55ca6a1b5d4d75050ccbaa
diksha2150 <dwivediakanksha74@gmail.com>
2025-01-31 12:58:55 +0530

    rady to review

--- js/custom.js
+++ js/custom.js
@@ -4,34 +4,34 @@ jQuery(document).ready(function () {
         jQuery("body").toggleClass('menu-open');
     });
 });
+gsap.registerPlugin(ScrollTrigger);
 
-// gsap.registerPlugin(ScrollTrigger);
+const images = gsap.utils.toArray(".right-img img");
+const rightElements = gsap.utils.toArray(".left-cont .left-hub-card");
 
-// const images = gsap.utils.toArray(".left-content img");
-// const rightElements = gsap.utils.toArray(".right-content .right-element");
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".scroller-main",
+    start: "top 5%",
+    end: "+=300%",
+    pin: true,
+    scrub: true,
+    markers: false
+  }
+});
 
-// const tl = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: ".container",
-//     start: "top top",
-//     end: "+=300%",
-//     pin: true,
-//     scrub: true,
-//     markers: true
-//   }
-// });
+images.forEach((img, i) => {
+  if (images[i + 1]) {
+    // Creates delay between sections
+    //tl.to(img, { opacity: 0 }, "+=0.5")
 
-// images.forEach((img, i) => {
-//   if (images[i + 1]) {
-//     // Creates delay between sections
-//     //tl.to(img, { opacity: 0 }, "+=0.5")
+    // This is one after the other without pause
+    tl.to(img, { opacity: 0 })
+      .to(images[i + 1], { opacity: 1 }, "<")
+      .to(rightElements, { yPercent: -(100 * (i + 1)), ease: "none" }, "<");
+  }
+});
 
-//     // This is one after the other without pause
-//     tl.to(img, { opacity: 0 })
-//       .to(images[i + 1], { opacity: 1 }, "<")
-//       .to(rightElements, { yPercent: -(100 * (i + 1)), ease: "none" }, "<");
-//   }
-// });
+// Adds pause at the end of the timeline
+tl.to({}, {}, "+=0.5");
 
-// // Adds pause at the end of the timeline
-// tl.to({}, {}, "+=0.5");
